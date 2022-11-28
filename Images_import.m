e25e9c89895fa98014bf4bf0e11da68d0b180daa
celiacruz25 <celia.cruze@autonoma.cat>
2022-11-28 13:08:50 +0200

    Image Import uploaded

--- Images_import.m
+++ Images_import.m
@@ -1,29 +1,28 @@
 % Import images
 
-clear all;
-
-data_folder = "Data\";
-
-subfolders = genpath(data_folder);
-subfolders = split(subfolders, ";");
-subfolders = string(subfolders); 
-index = find(strlength(subfolders(:)) <= 10);
-for i = 0:(length(index)-1)
-    subfolders(index(end-i)) = [];
-end
-for i = 1:length(subfolders)
-    subfolders(i) = strcat(subfolders(i),"\");
-end
-% paths = [];
-k = 1;
-for i = 1:length(subfolders)
-    d = dir(subfolders(i));
-    for j = 3:length(d)
-        image_name = d(j).name;
-        paths(k) = strcat(subfolders(i),image_name);
-        k = k + 1;
+function paths = Images_import(data_folder)
+    subfolders = genpath(data_folder);
+    subfolders = split(subfolders, ";");
+    subfolders = string(subfolders);
+    index = find(strlength(subfolders(:)) <= strlength(data_folder));
+    
+    for i = 0:(length(index)-1)
+        subfolders(index(end-i)) = [];
+    end
+    
+    for i = 1:length(subfolders)
+        subfolders(i) = strcat(subfolders(i),"\");
+    end
+    
+    
+    for i = 1:length(subfolders)
+        d = dir(subfolders(i));
+        k = 1;
+        for j = 3:length(d)
+            image_name = d(j).name;
+            paths(i,k) = strcat(subfolders(i),image_name);
+            k = k + 1;
+        end
     end
 end
 
-% To read the images we only will have to do:
-imread(paths(l));
