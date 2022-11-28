7207d3cf1b6574e21bfc8443121047504b35731d
celiacruz25 <celia.cruze@autonoma.cat>
2022-11-28 16:57:21 +0200

    Image Import uploaded + targets

--- Images_import.m
+++ Images_import.m
@@ -20,7 +20,7 @@ function paths = Images_import(data_folder)
         k = 1;
         for j = 3:length(d)
             image_name = d(j).name;
-            paths(i,k) = strcat(subfolders(i),image_name);
+            paths(i,k).path = strcat(subfolders(i),image_name);
             k = k + 1;
         end
     end
